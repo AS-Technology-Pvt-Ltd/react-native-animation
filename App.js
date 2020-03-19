8bf21064a2b1d32734fd940a2d3c9a72711b9167
Sachin Gupta <sachin.gupta@tutree.com>
2020-03-20 01:31:33 +0530

    update

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Opacity from "./src/Opacity";
+import TranslatePosition from "./src/TranslatePosition";
+import Scalling from "./src/Scaling";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Scalling />
     </View>
   );
 }
@@ -12,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
